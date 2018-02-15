720fb60eea6dc157d3d93ad0edf5b454a9f17d86
root <root@debian.jon>
2018-02-15 09:53:55 -0500

    practica uno

---
+++ index.php
@@ -0,0 +1,24 @@
+ 
+	
+<h2>Bienvenida</h2>
+
+<br />
+<p>
+Mision:<br />
+Proporcionar la infraestructura y servicios de Tecnologías de Información y Comunicaciones que requiere la Facultad para su buen funcionamiento en el desarrollo de las actividades académico-administrativas de la misma. 
+<br />
+Vision:<br />
+Ser un área de servicios que brinde de manera eficiente y oportuna soluciones de Tecnologías de Información y Comunicaciones que impulsen y favorezcan de manera continua el desarrollo e innovación tecnológica de la Facultad. 
+<br />
+Valores:<br />
+
+    Responsabilidad
+    Integridad y ética
+    Respeto
+    Actitud de servicio
+    Trabajo en equipo
+    Mejora continua
+    Compromiso social
+
+</p>
+	
---
+++ registro.php
@@ -0,0 +1,14 @@
+<h2>REgistro de usuarios</h2>
+<p>
+Todos los campos deben de ser obligaorios
+</p>
+
+<form action="registro.php" method="post">
+<p>nombre: <input type="text" name="nombre" /></p>
+<p>Apellido paterno: <input type="text" name="paterno" /></p>
+<p>Apellido materno: <input type="text" name="materno" /></p>
+<p>telefono: <input type="text" name="telefono" /></p>
+<p>correo: <input type="text" name="correo" /></p>
+<p><input type="submit" /></p>
+</form>
+
